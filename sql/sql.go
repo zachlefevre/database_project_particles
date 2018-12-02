4558bd449f8dddcda2aea8a9ea037764625c56ca
zachary lefevre <zachlefevre@gmail.com>
2018-12-02 14:30:43 -0600

    added persistence layer init

--- sql/sql.go
+++ sql/sql.go
@@ -1,5 +1,16 @@
 package sql
 
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	connectionstring = "postgres://root@localhost.com"
+)
+
 type PersistResponse struct {
 	isSucces bool
 }
@@ -14,4 +25,82 @@ func PersistWallCollisionEvent(pName string, wallName string, epoch int, timeste
 
 func PersistParticleLocation(pName string, epoch int, timestep int, x int, y int) (PersistResponse, error) {
 	return PersistResponse{}, nil
+}
+
+func createDatabase() {
+	db, err := sql.Open("postgres", connectionstring)
+	defer db.Close()
+	if err != nil {
+		log.Fatal("error connecting to the database: ", err)
+	}
+	sqlString := "CREATE DATABASE IF NOT EXISTS Knuth"
+	if resp, err := db.Exec(sqlString); err != nil {
+		log.Fatal("Failed to Execute"+sqlString, err)
+	} else {
+		log.Println("Executed sql string"+sqlString, resp)
+	}
+}
+func createParticleCollision() {
+	db, err := sql.Open("postgres", connectionstring)
+	defer db.Close()
+	if err != nil {
+		log.Fatal("error connecting to the database: ", err)
+	}
+	sqlString := `CREATE TABLE IF NOT EXISTS Knuth.particleCollision
+	(id UUID, p1 STRING, p2 STRING, epoch INT, timestep INT)`
+	if resp, err := db.Exec(sqlString); err != nil {
+		log.Fatal("Failed to Execute"+sqlString, err)
+	} else {
+		log.Println("Executed sql string"+sqlString, resp)
+	}
+}
+func createWallCollision() {
+	db, err := sql.Open("postgres", connectionstring)
+	defer db.Close()
+	if err != nil {
+		log.Fatal("error connecting to the database: ", err)
+	}
+	sqlString := `CREATE TABLE IF NOT EXISTS Knuth.wallCollision
+	(id UUID, particle STRING, obj STRING, epoch INT, timestep INT)`
+	if resp, err := db.Exec(sqlString); err != nil {
+		log.Fatal("Failed to Execute"+sqlString, err)
+	} else {
+		log.Println("Executed sql string"+sqlString, resp)
+	}
+}
+func createParticle() {
+	db, err := sql.Open("postgres", connectionstring)
+	defer db.Close()
+	if err != nil {
+		log.Fatal("error connecting to the database: ", err)
+	}
+	sqlString := `CREATE TABLE IF NOT EXISTS Knuth.particle
+	(name STRING, mass INT)`
+	if resp, err := db.Exec(sqlString); err != nil {
+		log.Fatal("Failed to Execute"+sqlString, err)
+	} else {
+		log.Println("Executed sql string"+sqlString, resp)
+	}
+}
+func createLocation() {
+	db, err := sql.Open("postgres", connectionstring)
+	defer db.Close()
+	if err != nil {
+		log.Fatal("error connecting to the database: ", err)
+	}
+	sqlString := `CREATE TABLE IF NOT EXISTS Knuth.location
+	(particle STRING, epoch INT, timestep INT, x INT, y INT)`
+	if resp, err := db.Exec(sqlString); err != nil {
+		log.Fatal("Failed to Execute"+sqlString, err)
+	} else {
+		log.Println("Executed sql string"+sqlString, resp)
+	}
+}
+
+func init() {
+	createDatabase()
+	createParticleCollision()
+	createWallCollision()
+	createParticle()
+	createLocation()
 }
